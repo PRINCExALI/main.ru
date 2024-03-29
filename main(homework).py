2ddba5e4711db6a22808175b851d9738f07fa7b9
Александр Поляков <164219631+PRINCExALI@users.noreply.github.com>
2024-03-29 09:12:39 +0300

    Add files via upload

---
+++ main(homework).py
@@ -0,0 +1,39 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+def test():
+    a = 5
+    b = 10
+    print(a, b)
+
+
+test()
+
+
+def test2(c, d, e):
+    print(c, d, e)
+
+
+test2(1, 2, 3)
+
+
+def test3(r=5, t=10, y=15):
+    print(r, t, y)
+
+
+test3()
